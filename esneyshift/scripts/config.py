c20660ae7437617464cdc3a21aa18e1696adbcad
JuanCBayona <116586410+JuanCBayona@users.noreply.github.com>
2026-06-10 14:06:44 -0500

    Create config.py

---
+++ esneyshift/scripts/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+SUPPORTED_EXTENSIONS = {
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".bmp",
+    ".tif",
+    ".tiff",
+    ".webp"
+}
+
+RANDOM_SEED = 42
+
+CACHE_DIR = Path("cache")
+
+PCA_COMPONENTS = 50
+
+IMAGE_SIZE = 224
+
+FEATURE_DIM = 1024
+
+UNI_CHECKPOINT = (
+    "/mnt/media2/JuanBayona/esneyshift/"
+    "modelos_fundacionales/UNI/pytorch_model.bin"
+)
